60da1399791ade6d1a5e205a7a250547c777f1ab
Lucas Rego <lucas.raziel@gmail.com>
2021-11-18 18:11:26 -0300

    tags for whatsapp

--- src/screens/Android.tsx
+++ src/screens/Android.tsx
@@ -1,13 +1,30 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
+import Helmet from 'react-helmet';
 
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>This will redirect to Android page</p>
-      </header>
-    </div>
-  );
+interface Props {
+  item?: {
+    name: string;
+    image: string;
+  };
 }
+
+const App: React.FC<Props> = ({ item }) => (
+  <div className="App">
+    {item && (
+      <Helmet>
+        <meta property="og:site_name" content="Appzone" />
+        <meta property="og:title" content={item.name} />
+        {/* <meta property="og:description" content="Programa de fiestas" /> */}
+        <meta property="og:image" content={item.image} />
+        <meta property="og:type" content="website" />
+        <meta property="og:updated_time" content="1440432930" />
+      </Helmet>
+    )}
+    <header className="App-header">
+      <p>This will redirect to Android page</p>
+    </header>
+  </div>
+);
 
 export default App;
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Android from './Android';
 import Desktop from './Desktop';
 import Ios from './Ios';
+import api from '../services/api';
+
+interface Item {
+  name: string;
+  image: string;
+}
 
 function App(): JSX.Element {
+  const location = useLocation();
+  const [item, setItem] = React.useState<Item | undefined>();
   const isAndroid = navigator.userAgent.toLowerCase().indexOf('android') > -1;
   const isIOS = navigator.userAgent.toLowerCase().indexOf('iphone') > -1;
   const isNotAnyMobile = !isAndroid && !isIOS;
+  useEffect(() => {
+    if (location.pathname.includes('groups')) {
+      if (location.pathname.split('/').length >= 3) {
+        const groupId = location.pathname.split('/')[2];
+        api.get(`groups/${groupId}`).then(({ data }) => {
+          setItem({ name: data.name, image: data.photoURL });
+        });
+      }
+    }
+    if (location.pathname.includes('users')) {
+      if (location.pathname.split('/').length >= 3) {
+        const groupId = location.pathname.split('/')[2];
+        api.get(`users/${groupId}`).then(({ data }) => {
+          setItem({ name: data.name, image: data.photoURL });
+        });
+      }
+    }
+  }, [location]);
   return (
     <>
-      {isAndroid && <Android />}
-      {isIOS && <Ios />}
-      {isNotAnyMobile && <Desktop />}
+      {isAndroid && <Android item={item} />}
+      {isIOS && <Ios item={item} />}
+      {isNotAnyMobile && <Desktop item={item} />}
     </>
   );
 }
